d537eafdced07f61d33162e711dd3b1e4d027ac2
sam <sam.prager@gmail.co6m>
2015-08-07 04:35:20 -0500

    make themes class based

---
+++ src/play/game/router.js
@@ -0,0 +1,55 @@
+import {inject} from 'aurelia-framework';
+import {GameState} from './gameState';
+import {GameThemes} from './gameThemes';
+import {GameStyles} from './gameStyles';
+
+
+@inject( GameState, GameStyles, GameThemes) 
+export class Router{
+
+    constructor( state, styleAdapter, themeAdapter){                    
+       this.gameState = state;
+       this.styleAdapter = styleAdapter;
+       this.themeAdapter = themeAdapter;
+       
+        //load story data from gamestate
+        this.story = state.story;
+        this.styles = styleAdapter.styles;         
+    }
+    
+    configureRouter(config, router){
+        config.map([
+            
+            { route: ['','cover'], name: 'begin',  moduleId: './cover', nav: false } ,
+            { route: 'page/:id', name: 'page',  moduleId: './play', nav: false } 
+            
+        ]); 
+
+        this.router = router;
+    }
+    
+    activate(){
+        return this.styleAdapter.loadStyles().then( response => {
+            //apply styles
+           this.styles = response;
+           this.themeAdapter.applyTheme( this.styles.theme ); 
+           this.styleAdapter.applyStyles( this.styles );
+         });
+    }
+    
+    canDeactivate(){
+        //grab styles from body
+        this.styleAdapter.storeStyles( this.styles );
+        this.styleAdapter.removeStyles();
+        this.themeAdapter.removeTheme();
+        return true;   
+    }
+
+     
+    cycleTheme(){
+        var theme = this.themeAdapter.cycleTheme();
+        this.styles.theme = theme;
+    }
+ 
+
+}
